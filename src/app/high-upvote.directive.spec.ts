7614e0c4660b9ee73a8402607ff3e6e2b4485a5e
nicholas-oduor <oduor5742@gmail.com>
2021-01-31 13:33:50 +0300

    deployed to gh-pages

---
+++ src/app/high-upvote.directive.spec.ts
@@ -0,0 +1,8 @@
+import { HighUpvoteDirective } from './high-upvote.directive';
+
+describe('HighUpvoteDirective', () => {
+  it('should create an instance', () => {
+    const directive = new HighUpvoteDirective();
+    expect(directive).toBeTruthy();
+  });
+});
